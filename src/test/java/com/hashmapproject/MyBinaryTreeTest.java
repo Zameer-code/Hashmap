7f48df765cf31960b8f205ebbace26adac80e6cf
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-15 14:22:01 +0530

    Binary Tree test

---
+++ src/test/java/com/hashmapproject/MyBinaryTreeTest.java
@@ -0,0 +1,56 @@
+package com.BinaryTree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+	
+	@Test
+	 public void given3NumbersWhenAddedToBSTShouldReturnSize3() {
+		 MyBinaryTree<Integer>myBinaryTree = new MyBinaryTree<>();
+		 myBinaryTree.add(56);
+		 myBinaryTree.add(30);
+		 myBinaryTree.add(70);
+		 int size = myBinaryTree.getSize();
+		 Assert.assertEquals(3, size);
+	 }
+	 @Test
+	    public void givenNumbersWhenAddedToBinaryTreeShowsReturnSizeTrue() {
+	        MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<>();
+	        myBinaryTree.add(56);
+	        myBinaryTree.add(30);
+	        myBinaryTree.add(70);
+	        myBinaryTree.add(22);
+	        myBinaryTree.add(40);
+	        myBinaryTree.add(60);
+	        myBinaryTree.add(95);
+	        myBinaryTree.add(11);
+	        myBinaryTree.add(65);
+	        myBinaryTree.add(3);
+	        myBinaryTree.add(16);
+	        myBinaryTree.add(63);
+	        myBinaryTree.add(67);
+	        int size =myBinaryTree.getSize();
+	        Assert.assertEquals(13, size);
+	    }
+
+	    @Test
+	    public void givenNumbersAddedToBST_WhenSearched_ShouldReturn_True() {
+	        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+	        myBinaryTree.add(56);
+	        myBinaryTree.add(30);
+	        myBinaryTree.add(70);
+	        myBinaryTree.add(22);
+	        myBinaryTree.add(40);
+	        myBinaryTree.add(60);
+	        myBinaryTree.add(95);
+	        myBinaryTree.add(11);
+	        myBinaryTree.add(65);
+	        myBinaryTree.add(3);
+	        myBinaryTree.add(16);
+	        myBinaryTree.add(63);
+	        myBinaryTree.add(67);
+	        boolean result = myBinaryTree.search(60);
+	        Assert.assertTrue(result);
+	    }
+}
